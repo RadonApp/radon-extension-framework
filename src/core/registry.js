deb48b85106c99219fa32dc18dcbca41b2f29f31
Dean Gardiner <me@dgardiner.net>
2016-09-12 21:43:50 +1200

    Updated plugin registry class

--- src/core/registry.js
+++ src/core/registry.js
@@ -1,10 +1,101 @@
+export var OptionTypes = [
+    'checkbox',
+    'enable',
+    'slider'
+];
+
 export class Registry {
     constructor() {
+        this.plugins = {};
+        this.pluginsByType = {};
 
+        this.services = {};
+        this.servicesByType = {};
     }
 
-    register(plugin) {
-        console.log('register', plugin);
+    getPluginById(id) {
+        return this.plugins[id] || null;
+    }
+
+    listPlugins(type, options) {
+        options = options || {};
+        options.disabled = typeof options.disabled !== 'undefined' ? options.disabled : false;
+
+        if(typeof this.pluginsByType[type] === 'undefined') {
+            return [];
+        }
+
+        var plugins = [];
+
+        for(var key in this.pluginsByType[type]) {
+            if(!this.pluginsByType[type].hasOwnProperty(key)) {
+                continue;
+            }
+
+            // Retrieve plugin
+            var plugin = this.pluginsByType[type][key];
+
+            // Filter by enabled state
+            if(!plugin.enabled && options.disabled != true) {
+                continue;
+            }
+
+            // Append to result list
+            plugins.push(plugin);
+        }
+
+        return plugins;
+    }
+
+    registerPlugin(plugin) {
+        // Ensure plugin type object has been created
+        if(typeof this.pluginsByType[plugin.type] === 'undefined') {
+            this.pluginsByType[plugin.type] = {};
+        }
+
+        // Ensure plugin hasn't already been registered
+        if(typeof this.plugins[plugin.id] !== 'undefined') {
+            return false;
+        }
+
+        if(typeof this.pluginsByType[plugin.type][plugin.id] !== 'undefined') {
+            return false;
+        }
+
+        // Register plugin
+        this.plugins[plugin.id] = plugin;
+        this.pluginsByType[plugin.type][plugin.id] = plugin;
+
+        console.log('Registered plugin: ' + plugin.id);
+        return true;
+    }
+
+    registerService(service) {
+        // Ensure plugin type object has been created
+        if(typeof this.servicesByType[service.type] === 'undefined') {
+            this.servicesByType[service.type] = {};
+        }
+
+        // Ensure service hasn't already been registered
+        if(typeof this.services[service.id] !== 'undefined') {
+            console.warn('Service "' + service.id + '" has already been registered');
+            return false;
+        }
+
+        if(typeof this.servicesByType[service.type][service.id] !== 'undefined') {
+            console.warn('Service "' + service.id + '" has already been registered');
+            return false;
+        }
+
+        // Register service
+        this.services[service.id] = service;
+        this.servicesByType[service.type][service.id] = service;
+
+        console.log('Registered service: ' + service.id);
+
+        // Ensure plugin has been registered
+        this.registerPlugin(service.plugin);
+        return true;
     }
 }
 
