4b43b409c2cbcfaa61da8c0f4778d15f64b1eddb
Dean Gardiner <me@dgardiner.net>
2016-09-23 20:23:57 +1200

    Converted event bus to an ES6 class

--- src/core/bus.js
+++ src/core/bus.js
@@ -1,82 +1,81 @@
 import {Messaging} from 'eon.extension.browser';
 
 import EventEmitter from 'eventemitter3';
-import util from 'util';
 
 
-function Bus() {
-    this.type = null;
-}
+class Bus extends EventEmitter {
+    constructor() {
+        super();
 
-util.inherits(Bus, EventEmitter);
-
-Bus.prototype.configure = function(type) {
-    this.type = type;
-
-    // Initialize event bus
-    this.initialize();
-};
-
-Bus.prototype.initialize = function() {
-    var self = this;
-
-    if(this.type.indexOf('background/') === 0) {
-        // Emit received messages to the event bus
-        Messaging.addListener(function(message) {
-            if(message.type !== 'bus') {
-                return;
-            }
-
-            var args = [message.event].concat(
-                message.arguments
-            );
-
-            // Emit message to the event bus
-            Bus.super_.prototype.emit.apply(self, args);
-        });
+        this.type = null;
     }
 
-    console.debug('Initialized event bus (type: %o)', this.type);
-};
+    configure(type) {
+        this.type = type;
 
-Bus.prototype.emit = function(event, a1, a2, a3, a4, a5) {
-    // Send messages to the background page
-    this.send(event, Array.from(arguments).splice(1));
-
-    // Fire event listeners
-    Bus.super_.prototype.emit.apply(this, arguments);
-};
-
-Bus.prototype.send = function(event, args) {
-    if(this.type === null || this.type.indexOf('background/') === 0) {
-        console.debug('Ignoring send() on event bus with type: %s', this.type);
-        return;
+        // Initialize event bus
+        this.initialize();
     }
 
-    // Cleanup event arguments
-    var cleanedArgs = [];
+    initialize() {
+        if(this.type.indexOf('background/') === 0) {
+            // Emit received messages to the event bus
+            Messaging.addListener((message) => {
+                if(message.type !== 'bus') {
+                    return;
+                }
 
-    for(var i = 0; i < args.length; ++i) {
-        var arg = args[i];
+                var args = [message.event].concat(
+                    message.arguments
+                );
 
-        if(typeof arg === 'undefined') {
-            break;
+                // Emit message to the event bus
+                super.emit.apply(this, args);
+            });
         }
 
-        cleanedArgs.push(arg);
+        console.debug('Initialized event bus (type: %o)', this.type);
     }
 
-    // Build message
-    var message = {
-        type: 'bus',
+    emit(event, a1, a2, a3, a4, a5) {
+        // Send messages to the background page
+        this.send(event, Array.from(arguments).splice(1));
 
-        event: event,
-        arguments: cleanedArgs
-    };
+        // Fire event listeners
+        super.emit.apply(this, arguments);
+    }
 
-    // Send message to background script
-    console.debug('Sending message to background script: %O', message);
-    Messaging.send(message);
-};
+    send(event, args) {
+        if(this.type === null || this.type.indexOf('background/') === 0) {
+            console.debug('Ignoring send() on event bus with type: %s', this.type);
+            return;
+        }
+
+        // Cleanup event arguments
+        var cleanedArgs = [];
+
+        for(var i = 0; i < args.length; ++i) {
+            var arg = args[i];
+
+            if(typeof arg === 'undefined') {
+                break;
+            }
+
+            cleanedArgs.push(arg);
+        }
+
+        // Build message
+        var message = {
+            type: 'bus',
+
+            event: event,
+            arguments: cleanedArgs
+        };
+
+        // Send message to background script
+        console.debug('Sending message to background script: %O', message);
+        Messaging.send(message);
+    }
+}
 
 export default new Bus();
