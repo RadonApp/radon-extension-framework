0deaf88bd728d563889a9dbf9936043363379b86
Dean Gardiner <me@dgardiner.net>
2016-09-26 16:01:37 +1300

    Added event handling to the `ScrobbleService` class

--- src/services/destination/scrobble.js
+++ src/services/destination/scrobble.js
@@ -2,7 +2,53 @@ import Service from '../base';
 
 
 export default class ScrobbleService extends Service {
-    constructor(plugin) {
+    constructor(plugin, accepts) {
         super(plugin, 'scrobble', 'destination/scrobble');
+
+        this.accepts = accepts;
+    }
+
+    onSessionUpdated(event, session) {
+        try {
+            if(event === 'created') {
+                return this.onCreated(session);
+            }
+
+            if(event === 'started') {
+                return this.onStarted(session);
+            }
+
+            if(event === 'progress') {
+                return this.onProgress(session);
+            }
+
+            if(event === 'paused') {
+                return this.onPaused(session);
+            }
+
+            if(event === 'ended') {
+                return this.onEnded(session);
+            }
+
+            console.warn('Invalid activity event received: %o', event);
+            return false;
+        } catch(e) {
+            console.error('Unable to process session event %o:', event, e);
+        }
+    }
+
+    onCreated(session) {}
+    onProgress(session) {}
+
+    onStarted(session) {
+        throw new Error('Not Implemented');
+    }
+
+    onPaused(session) {
+        throw new Error('Not Implemented');
+    }
+
+    onEnded(session) {
+        throw new Error('Not Implemented');
     }
 }
