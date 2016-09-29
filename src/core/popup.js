1c2fa516521d1abfc0324709510de8d4bfdb21c7
Dean Gardiner <me@dgardiner.net>
2016-09-29 16:02:37 +1300

    Implemented the `Popup` and `WindowMessaging` classes

---
+++ src/core/popup.js
@@ -0,0 +1,147 @@
+import {WindowMessaging} from '../messaging/window';
+
+import merge from 'lodash-es/merge';
+import uuid from 'uuid';
+
+var WINDOW_FEATURES = [
+    'width',
+    'height',
+    'left',
+    'top'
+];
+
+
+export default class Popup {
+    constructor(name, handle) {
+        this.name = name;
+        this.handle = handle;
+
+        this.messaging = new WindowMessaging(name, handle);
+    }
+
+    static open(url, target, options) {
+        options = typeof options !== 'undefined' ? options : {};
+
+        // Set `options` defaults
+        options = merge({
+            position: null,
+            width: 500,
+            height: 500,
+            left: 0,
+            top: 0,
+
+            offsetLeft: 0,
+            offsetTop: 0,
+
+            timeout: 5 * 60
+        }, options);
+
+        // Process "position" option
+        if(options.position === 'center') {
+            delete options['position'];
+
+            options.left = window.screenLeft + (window.outerWidth / 2) - (options.width / 2) + options.offsetLeft;
+            options.top = window.screenTop + options.offsetTop;
+        }
+
+        // Construct popup promise
+        return new Promise(function(resolve, reject) {
+            var name = 'eon.popup/' + uuid.v4();
+            var features = Popup._buildFeaturesString(options);
+
+            console.debug('Opening popup %o (name: %o, features: %o)', url, name, features);
+
+            // Open empty popup, then navigate to `url` (note: fixes `window.opener` bug in chrome)
+            var handle = window.open('', name, features);
+            handle.location.href = url;
+
+            // Move to position
+            handle.moveTo(options.left, options.top);
+
+            // Construct popup
+            var popup = new Popup(name, handle);
+            var done = false;
+
+            // Bind to messaging events
+            popup.messaging.on('resolve', (result) => {
+                done = true;
+
+                // Close window
+                handle.close();
+
+                // Resolve promise
+                resolve(result);
+            });
+
+            popup.messaging.on('reject', (message) => {
+                done = true;
+
+                // Remove popup event listeners
+                popup.messaging.removeAllListeners();
+
+                // Reject promise
+                reject(new Error(message));
+            });
+
+            // Watch for window closes
+            function checkPopupClosed() {
+                if(done) {
+                    return;
+                }
+
+                console.debug('Checking if the popup has been closed...');
+
+                if(handle.closed) {
+                    done = true;
+
+                    // Remove popup event listeners
+                    popup.messaging.removeAllListeners();
+
+                    // Reject promise
+                    reject(new Error('Popup closed'));
+                }
+
+                setTimeout(checkPopupClosed, 2000);
+            }
+
+            setTimeout(checkPopupClosed, 5000);
+
+            // Start timeout callback
+            setTimeout(() => {
+                if(done) {
+                    return;
+                }
+
+                done = true;
+
+                // Remove popup event listeners
+                popup.messaging.removeAllListeners();
+
+                // Ensure popup has been closed
+                if(!handle.closed) {
+                    handle.close();
+                }
+
+                // Reject promise
+                reject(new Error('Popup timed out, response wasn\'t returned in ' + options.timeout + ' second(s)'));
+            }, options.timeout * 1000);
+        });
+    }
+
+    static _buildFeaturesString(features) {
+        var keys = Object.keys(features);
+        var fragments = [], i;
+
+        for(i = 0; i < keys.length; ++i) {
+            var key = keys[i];
+
+            if(WINDOW_FEATURES.indexOf(key) === -1) {
+                continue;
+            }
+
+            fragments.push(key + '=' + features[key]);
+        }
+
+        return fragments.join(',');
+    }
+}
---
+++ src/messaging/window.js
@@ -0,0 +1,80 @@
+import EventEmitter from 'eventemitter3';
+
+
+export class WindowMessaging extends EventEmitter {
+    constructor(name, target, origin) {
+        super();
+
+        this.name = name || window.name;
+        this.target = target || window.opener || window.parent;
+        this.origin = origin || window.location.origin;
+
+        // Bind to messages
+        window.addEventListener('message', (e) => {
+            var message = e.data;
+
+            // Verify message type
+            if(typeof message.type === 'undefined' || message.type !== 'window') {
+                console.warn('Received an invalid message:', message);
+                return;
+            }
+
+            // Verify message matches popup `name`
+            if(message.window.name !== this.name) {
+                console.debug('Ignoring message %O doesn\'t match popup name: %s', message, this.name);
+                return;
+            }
+
+            console.debug('[%s] Received message: %O', this.name, message);
+
+            // Build `emit()` arguments
+            var args = [message.event].concat(
+                message.arguments
+            );
+
+            // Emit message to listeners
+            super.emit.apply(this, args);
+        });
+    }
+
+    emit(event, a1, a2, a3, a4, a5) {
+        // Send messages to the background page
+        this.send(event, Array.from(arguments).splice(1));
+
+        // Fire event listeners
+        super.emit.apply(this, arguments);
+    }
+
+    send(event, args) {
+        // Cleanup event arguments
+        var cleanedArgs = [];
+
+        for(var i = 0; i < args.length; ++i) {
+            var arg = args[i];
+
+            if(typeof arg === 'undefined') {
+                break;
+            }
+
+            cleanedArgs.push(arg);
+        }
+
+        // Build message
+        var message = {
+            type: 'window',
+
+            window: {
+                name: this.name
+            },
+
+            event: event,
+            arguments: cleanedArgs
+        };
+
+        // Send message to background script
+        console.debug('Sending message to %o: %O', message);
+        this.target.postMessage(message, this.origin);
+    }
+}
+
+export default new WindowMessaging();
