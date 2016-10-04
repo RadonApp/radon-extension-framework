e0155f9315d9ee0a218b029201111bad1f263c0e
Dean Gardiner <me@dgardiner.net>
2016-10-04 23:22:19 +1300

    Added the `BrowserDeclarativeContent` base class

---
+++ src/base/browser/declarativeContent.js
@@ -0,0 +1,12 @@
+import {NotImplementedException} from '../../core/exceptions';
+
+
+export default class BrowserDeclarativeContent {
+    addRules(rules) {
+        throw new NotImplementedException();
+    }
+
+    removeRules(ruleIds) {
+        throw new NotImplementedException();
+    }
+}
