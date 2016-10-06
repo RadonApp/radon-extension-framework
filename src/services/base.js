bd2864603314f42cba35a3e93e3b307bf606cf08
Dean Gardiner <me@dgardiner.net>
2016-10-06 18:46:58 +1300

    Removed the old `Service.emit()` method

--- src/services/base.js
+++ src/services/base.js
@@ -1,6 +1,3 @@
-import Bus from '../core/bus';
-
-
 export default class Service {
     constructor(plugin, key, type) {
         this.plugin = plugin;
@@ -30,11 +27,5 @@ export default class Service {
 
     initialize() {
         this._initialized = true;
-    }
-
-    emit(type, a1, a2, a3, a4, a5) {
-        type = this.key + '.' + type;
-
-        return Bus.emit(type, a1, a2, a3, a4, a5);
     }
 }
