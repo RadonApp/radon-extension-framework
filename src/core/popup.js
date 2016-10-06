bc9c97368e05fcda8211e0db698515fbb7343ba8
Dean Gardiner <me@dgardiner.net>
2016-10-06 18:46:45 +1300

    Switched `Popup` to use the new message bus implementation, and cleaned up the entire class structure

--- src/core/popup.js
+++ src/core/popup.js
@@ -1,5 +1,6 @@
-import {WindowMessaging} from '../messaging/window';
+import MessagingBus from '../messaging/bus';
 
+import EventEmitter from 'eventemitter3';
 import merge from 'lodash-es/merge';
 import uuid from 'uuid';
 
@@ -11,19 +12,20 @@ const WINDOW_FEATURES = [
 ];
 
 
-export default class Popup {
-    constructor(name, handle) {
+export default class Popup extends EventEmitter {
+    constructor(name, options) {
+        super();
+
         this.name = name;
-        this.handle = handle;
+        this.options = options;
 
-        this.messaging = new WindowMessaging(name, handle);
-    }
+        this.handle = null;
 
-    static open(url, target, options) {
-        options = typeof options !== 'undefined' ? options : {};
+        this.resolved = false;
+        this.rejected = false;
 
-        // Set `options` defaults
-        options = merge({
+        // Set default options
+        this.options = merge({
             position: null,
             width: 500,
             height: 500,
@@ -34,102 +36,151 @@ export default class Popup {
             offsetTop: 0,
 
             timeout: 5 * 60
-        }, options);
+        }, options || {});
 
         // Process "position" option
-        if(options.position === 'center') {
-            delete options['position'];
+        if(this.options.position === 'center') {
+            delete this.options['position'];
 
-            options.left = window.screenLeft + (window.outerWidth / 2) - (options.width / 2) + options.offsetLeft;
-            options.top = window.screenTop + options.offsetTop;
+            this.options.left = (
+                window.screenLeft + (window.outerWidth / 2) -
+                (this.options.width / 2) + this.options.offsetLeft
+            );
+
+            this.options.top = window.screenTop + this.options.offsetTop;
         }
 
-        // Construct popup promise
+        // Build window features string
+        this._features = this._buildFeaturesString(this.options);
+
+        // Connect to relay messaging bus
+        this._bus = new MessagingBus(name).connect(
+            'eon.extension.core:relay'
+        );
+
+        // Bind to messaging events
+        this._bus.on('popup.resolve', this._onResolved.bind(this));
+        this._bus.on('popup.reject', this._onRejected.bind(this));
+
+        // Start monitoring close state
+        setTimeout(this._checkClosed.bind(this), 5000);
+
+        // Start timeout trigger
+        setTimeout(this._onTimeout.bind(this), this.options.timeout * 1000);
+    }
+
+    get closed() {
+        return this.handle.closed;
+    }
+
+    get complete() {
+        return this.resolved || this.rejected;
+    }
+
+    // region Public methods
+
+    open(url) {
+        console.debug('Opening popup %o (name: %o, features: %o)', url, this.name, this._features);
+
+        // Open empty popup, then navigate to `url` (note: fixes `window.opener` bug in chrome)
+        this.handle = window.open('', this.name, this._features);
+        this.handle.location.href = url;
+
+        // Move popup to specified position
+        this.handle.moveTo(this.options.left, this.options.top);
+    }
+
+    close() {
+        if(this.handle.closed) {
+            return;
+        }
+
+        // Close popup window
+        this.handle.close();
+    }
+
+    dispose() {
+        // Remove message event listeners
+        this._bus.removeAllListeners();
+
+        // Disconnect messaging channels
+        this._bus.disconnectAll();
+    }
+
+    // endregion
+
+    // region Static methods
+
+    static open(url, options) {
         return new Promise(function(resolve, reject) {
             let name = 'eon.popup/' + uuid.v4();
-            let features = Popup._buildFeaturesString(options);
+            let popup = new Popup(name, options);
 
-            console.debug('Opening popup %o (name: %o, features: %o)', url, name, features);
+            // Open popup window
+            popup.open(url);
 
-            // Open empty popup, then navigate to `url` (note: fixes `window.opener` bug in chrome)
-            let handle = window.open('', name, features);
-
-            handle.location.href = url;
-
-            // Move to position
-            handle.moveTo(options.left, options.top);
-
-            // Construct popup
-            let popup = new Popup(name, handle);
-            let done = false;
-
-            // Bind to messaging events
-            popup.messaging.on('resolve', (result) => {
-                done = true;
-
-                // Close window
-                handle.close();
-
-                // Resolve promise
-                resolve(result);
-            });
-
-            popup.messaging.on('reject', (message) => {
-                done = true;
-
-                // Remove popup event listeners
-                popup.messaging.removeAllListeners();
-
-                // Reject promise
-                reject(new Error(message));
-            });
-
-            // Watch for window closes
-            function checkPopupClosed() {
-                if(done) {
-                    return;
-                }
-
-                console.debug('Checking if the popup has been closed...');
-
-                if(handle.closed) {
-                    done = true;
-
-                    // Remove popup event listeners
-                    popup.messaging.removeAllListeners();
-
-                    // Reject promise
-                    reject(new Error('Popup closed'));
-                }
-
-                setTimeout(checkPopupClosed, 2000);
-            }
-
-            setTimeout(checkPopupClosed, 5000);
-
-            // Start timeout callback
-            setTimeout(() => {
-                if(done) {
-                    return;
-                }
-
-                done = true;
-
-                // Remove popup event listeners
-                popup.messaging.removeAllListeners();
-
-                // Ensure popup has been closed
-                if(!handle.closed) {
-                    handle.close();
-                }
-
-                // Reject promise
-                reject(new Error('Popup timed out, response wasn\'t returned in ' + options.timeout + ' second(s)'));
-            }, options.timeout * 1000);
+            // Bind popup events to promise
+            popup.on('resolve', resolve);
+            popup.on('reject', reject);
         });
     }
 
-    static _buildFeaturesString(features) {
+    // endregion
+
+    // region Private methods
+
+    _checkClosed() {
+        if(this.complete) {
+            return;
+        }
+
+        console.debug('Checking if popup has been closed...');
+
+        if(this.closed) {
+            // Popup rejected
+            this._onRejected('Popup closed');
+            return;
+        }
+
+        setTimeout(this._checkClosed.bind(this), 2000);
+    }
+
+    _onTimeout() {
+        if(this.complete) {
+            return;
+        }
+
+        // Popup rejected
+        this._onRejected('Popup timed out, response wasn\'t returned in ' + this.options.timeout + ' second(s)');
+    }
+
+    _onResolved(result) {
+        this.resolved = true;
+
+        // Ensure popup is closed
+        this.close();
+
+        // Dispose resources
+        this.dispose();
+
+        // Fire "resolved" event
+        this.emit('resolve', result);
+    }
+
+    _onRejected(message) {
+        this.rejected = true;
+
+        // Ensure popup is closed
+        this.close();
+
+        // Dispose resources
+        this.dispose();
+
+        // Fire "rejected" event
+        this.emit('reject', new Error(message));
+    }
+
+    _buildFeaturesString(features) {
         let keys = Object.keys(features);
         let fragments = [];
 
@@ -145,4 +196,6 @@ export default class Popup {
 
         return fragments.join(',');
     }
+
+    // endregion
 }
