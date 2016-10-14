055fc4c507b4a06e5cba21e87d242accce882f53
Dean Gardiner <me@dgardiner.net>
2016-10-14 15:46:19 +1300

    Added `generateRandomString()` method to the [core/helpers] module

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -1,5 +1,15 @@
 let getType = {};
 
+export function generateRandomString(length, chars) {
+    let result = '';
+
+    for(let i = length; i > 0; --i) {
+        result += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    return result;
+}
+
 export function isDefined(value) {
     return typeof value !== 'undefined' && value !== null;
 }
