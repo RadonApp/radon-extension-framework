f4d9ba9c956a32e3e90e202ca419683fe24d9d4a
Dean Gardiner <me@dgardiner.net>
2016-10-19 10:54:21 +1300

    Implemented select/drop-down configuration option model

---
+++ src/services/configuration/models/options/select.js
@@ -0,0 +1,16 @@
+import Preferences from 'eon.extension.browser/preferences';
+
+import {Option} from './core/base';
+
+
+export default class SelectOption extends Option {
+    constructor(plugin, key, label, choices, options) {
+        super(plugin, 'select', key, label, options);
+
+        this.options.choices = choices;
+    }
+
+    get() {
+        return Preferences.getString(this.id);
+    }
+}
