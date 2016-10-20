9c2d46e738e3bf203361a72f8dea779d42647083
Dean Gardiner <me@dgardiner.net>
2016-10-21 11:12:17 +1300

    Added the `hasClass` and `hasClassTree` helper methods

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -10,6 +10,40 @@ export function generateRandomString(length, chars) {
     return result;
 }
 
+export function hasClass(node, className) {
+    return node.classList.contains(className);
+}
+
+export function hasClassTree(node) {
+    let args = Array.from(arguments).slice(1);
+    let current = node;
+
+    for(let i = 0; i < args.length; ++i) {
+        // Ensure parent exists
+        if(!isDefined(current)) {
+            return false;
+        }
+
+        // Check if `current` matches class
+        let className = args[i];
+
+        if(!isDefined(className)) {
+            // Switch to parent node
+            current = current.parentNode;
+            continue;
+        }
+
+        if(!isDefined(current.classList) || !current.classList.contains(className)) {
+            return false;
+        }
+
+        // Switch to parent node
+        current = current.parentNode;
+    }
+
+    return true;
+}
+
 export function isDefined(value) {
     return typeof value !== 'undefined' && value !== null;
 }
