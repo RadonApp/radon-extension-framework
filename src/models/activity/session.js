84c017922c6ef3c6ede04bada1141fff95be00c6
Dean Gardiner <me@dgardiner.net>
2016-10-22 17:36:29 +1300

    Cleaned up the `Session` model, and renamed the "ended" event to "stopped"

--- src/models/activity/session.js
+++ src/models/activity/session.js
@@ -1,30 +1,53 @@
-import {round} from 'eon.extension.framework/core/helpers';
+/* eslint-disable no-multi-spaces, key-spacing */
+import {isDefined, round} from 'eon.extension.framework/core/helpers';
 
 export const SessionState = {
-    null: 0,
-    stalled: 1,
-    loading: 2,
+    created:    'created',
+    stalled:    'stalled',
+    playing:    'playing',
 
-    playing: 3,
+    pausing:    'pausing',
+    paused:     'paused',
 
-    pausing: 4,
-    paused: 5,
-
-    ended: 6
+    ended:      'ended'
 };
 
 
 export default class Session {
-    constructor(source, key, item, state) {
-        this.source = typeof source !== 'undefined' ? source : null;
+    constructor(source, key, options) {
+        // Validate parameters
+        if(!isDefined(source)) {
+            throw new Error('Invalid value provided for the "source" parameter');
+        }
 
-        this.key = typeof key !== 'undefined' ? key : null;
-        this.item = typeof item !== 'undefined' ? item : null;
+        if(!isDefined(key)) {
+            throw new Error('Invalid value provided for the "key" parameter');
+        }
 
-        this.state = typeof state !== 'undefined' ? state : SessionState.null;
+        this.source = source;
+        this.key = key;
 
+        // Parse options
+        if(!isDefined(options)) {
+            options = {};
+        }
+
+        this.identifier = isDefined(options.identifier) ?
+            options.identifier :
+            null;
+
+        this.metadata = isDefined(options.metadata) ?
+            options.metadata :
+            null;
+
+        this.state = isDefined(options.state) ?
+            options.state :
+            SessionState.created;
+
+        // Timing samples
         this.samples = [];
 
+        // Stalled status
         this.stalledAt = null;
         this.stalledPreviousState = null;
     }
@@ -38,26 +61,36 @@ export default class Session {
     }
 
     get progress() {
-        if(this.time === null || this.item === null || this.item.duration === null) {
+        if(this.time === null || this.metadata === null || this.metadata.duration === null) {
             return null;
         }
 
-        return round((this.time / this.item.duration) * 100, 2);
+        return round((this.time / this.metadata.duration) * 100, 2);
     }
 
     dump() {
         return {
             type: 'session',
-
+            source: this.source.dump(),
             key: this.key,
+
+            identifier: isDefined(this.identifier) ?
+                this.identifier.dump() :
+                null,
+
+            metadata: isDefined(this.metadata) ?
+                this.metadata.dump() :
+                null,
+
+            // Status
             state: this.state,
 
             time: this.time,
             progress: this.progress,
-            stalledAt: this.stalledAt,
 
-            item: this.item ? this.item.dump() : null,
-            source: this.source ? this.source.dump() : null
+            // Stalled status
+            stalledAt: this.stalledAt,
+            stalledPreviousState: this.stalledPreviousState
         };
     }
 }
