c19763770f618ae410e28843e9eb858a3659070e
Dean Gardiner <me@dgardiner.net>
2017-09-28 16:28:25 +1300

    Removed [manifest.json], and changed the framework plugin type to "core"

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -1,11 +1,9 @@
 import Plugin from 'neon-extension-framework/base/plugins/base';
 
-import Manifest from '../../manifest.json';
-
 
 export class MainPlugin extends Plugin {
     constructor() {
-        super('neon-extension', 'neon', Manifest);
+        super('neon-extension', 'core');
     }
 }
 
