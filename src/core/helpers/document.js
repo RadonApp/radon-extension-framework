6326c38f3ac5c373b85ac752ac5973b074f4a963
Dean Gardiner <me@dgardiner.net>
2017-10-19 12:57:22 +1300

    Added document helper methods (`awaitBody`, `awaitElement`, and `awaitElements`)

---
+++ src/core/helpers/document.js
@@ -0,0 +1,79 @@
+import {isDefined} from './value';
+
+
+
+export function awaitBody() {
+    return new Promise((resolve, reject) => {
+        if(isDefined(document.body)) {
+            resolve(document.body);
+            return;
+        }
+
+        // Wait for the "DOMContentLoaded" event to be fired
+        document.addEventListener('DOMContentLoaded', () => {
+            if(isDefined(document.body)) {
+                resolve(document.body);
+            } else {
+                reject(new Error('Body wasn\'t found after the "DOMContentLoaded" event'));
+            }
+        });
+    });
+}
+
+export function awaitElement(container, selector) {
+    return new Promise((resolve) => {
+        function check() {
+            let element = container.querySelector(selector);
+
+            if(!isDefined(element)) {
+                return false;
+            }
+
+            // Resolve promise
+            resolve(element);
+
+            return true;
+        }
+
+        // Check if the element exists
+        if(check()) {
+            return;
+        }
+
+        // Create observer
+        let observer = new MutationObserver(() => {
+            if(check()) {
+                observer.disconnect();
+            }
+        });
+
+        // Observe container for subtree changes
+        observer.observe(container, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+
+export function awaitElements(container, ...path) {
+    return new Promise((resolve, reject) => {
+        let current = container;
+        let pending = path.slice(0);
+
+        function next() {
+            awaitElement(current, pending.shift()).then((element) => {
+                if(pending.length < 1) {
+                    resolve(element);
+                    return;
+                }
+
+                next();
+            }, (err) => {
+                reject(err);
+            });
+        }
+
+        next();
+    });
+}
