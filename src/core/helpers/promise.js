c7c5915aa45aa3569044811aaca731c37e6bcdb0
Dean Gardiner <me@dgardiner.net>
2017-11-29 16:15:24 +1300

    Added the `resolveOne` helper function

--- src/core/helpers/promise.js
+++ src/core/helpers/promise.js
@@ -1,3 +1,25 @@
+export function resolveOne(items, target) {
+    return new Promise((resolve, reject) => {
+        let position = 0;
+
+        function next() {
+            if(position >= items.length) {
+                resolve(null);
+                return;
+            }
+
+            // Run next promise
+            target(items[position++]).then((result) => {
+                resolve(result);
+            }, () => {
+                next();
+            });
+        }
+
+        next();
+    });
+}
+
 export function runSequential(items, target) {
     return new Promise((resolve, reject) => {
         let position = 0;
@@ -20,5 +42,4 @@ export function runSequential(items, target) {
 
         next();
     });
-
 }
