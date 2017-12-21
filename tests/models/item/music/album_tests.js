49d096a6a2163f6021c1f58233f59bc318d03d49
Dean Gardiner <me@dgardiner.net>
2017-12-21 14:56:44 +1300

    Added `Album` item tests

---
+++ tests/models/item/music/album_tests.js
@@ -0,0 +1,191 @@
+import {Artist, Album} from 'neon-extension-framework/models/item/music';
+
+
+describe('Album', () => {
+    let artist = Artist.create('test', {
+        keys: {
+            id: '1'
+        },
+
+        title: 'Gorillaz'
+    });
+
+    describe('construct', function() {
+        it('instance', function() {
+            let item = new Album();
+
+            // Ensure title is null
+            expect(item.title).toBe(null);
+
+            // Ensure artist is null
+            expect(item.artist).toBe(null);
+        });
+
+        it('with values', function() {
+            let item = new Album({
+                title: 'Humanz'
+            });
+
+            // Ensure title is defined
+            expect(item.title).toBe('Humanz');
+            expect(item.resolve('test').title).toBe('Humanz');
+
+            // Ensure artist is null
+            expect(item.artist).toBe(null);
+        });
+
+        it('with children', function() {
+            let item = new Album({
+                title: 'Humanz',
+                artist
+            });
+
+            // Ensure title is defined
+            expect(item.title).toBe('Humanz');
+            expect(item.resolve('test').title).toBe('Humanz');
+
+            // Ensure artist title is defined
+            expect(item.artist.title).toBe('Gorillaz');
+            expect(item.artist.values.title).toBe('Gorillaz');
+            expect(item.artist.resolve('test').title).toBe('Gorillaz');
+        });
+    });
+
+    describe('create', function() {
+        it('instance', function() {
+            let item = Album.create('test');
+
+            // Ensure title is null
+            expect(item.title).toBe(null);
+
+            // Ensure artist is null
+            expect(item.artist).toBe(null);
+        });
+
+        it('with values', function() {
+            let item = Album.create('test', {
+                title: 'Humanz'
+            });
+
+            // Ensure title is defined
+            expect(item.title).toBe('Humanz');
+            expect(item.resolve('test').title).toBe('Humanz');
+
+            // Ensure artist is null
+            expect(item.artist).toBe(null);
+        });
+
+        it('with children', function() {
+            let item = Album.create('test', {
+                title: 'Humanz',
+                artist
+            });
+
+            // Ensure title is defined
+            expect(item.title).toBe('Humanz');
+            expect(item.resolve('test').title).toBe('Humanz');
+
+            // Ensure artist title is defined
+            expect(item.artist.title).toBe('Gorillaz');
+            expect(item.artist.values.title).toBe('Gorillaz');
+            expect(item.artist.resolve('test').title).toBe('Gorillaz');
+        });
+
+        it('validates the "source" parameter', function() {
+            expect(function() {
+                Album.create(5)
+            }).toThrow(
+                new Error('Invalid value provided for the "source" parameter (expected string)')
+            );
+        });
+    });
+
+    describe('createSelectors', function() {
+        it('with identifier', function() {
+            let selectors = Album.create('test', {
+                id: '2',
+                title: 'Humanz',
+                artist
+            }).createSelectors();
+
+            expect(selectors.length).toBe(1);
+
+            expect(selectors[0]).toEqual({
+                '_id': '2'
+            });
+        });
+
+        it('with artist identifier', function() {
+            let selectors = Album.create('test', {
+                keys: {
+                    id: '2'
+                },
+
+                title: 'Humanz',
+
+                // Artist
+                artist: new Artist({
+                    id: '1',
+                    title: 'Gorillaz'
+                })
+            }).createSelectors();
+
+            expect(selectors.length).toBe(2);
+
+            expect(selectors[0]).toEqual({
+                'type': 'music/album',
+                'keys.test.id': '2',
+
+                'artist._id': '1'
+            });
+
+            expect(selectors[1]).toEqual({
+                'type': 'music/album',
+                'keys.item.slug': 'humanz',
+
+                'artist._id': '1'
+            });
+        });
+
+        it('with keys', function() {
+            let selectors = Album.create('test', {
+                keys: {
+                    id: '2'
+                },
+
+                title: 'Humanz',
+                artist
+            }).createSelectors();
+
+            expect(selectors.length).toBe(4);
+
+            expect(selectors[0]).toEqual({
+                'type': 'music/album',
+                'keys.test.id': '2',
+
+                'artist.keys.test.id': '1'
+            });
+
+            expect(selectors[1]).toEqual({
+                'type': 'music/album',
+                'keys.test.id': '2',
+
+                'artist.keys.item.slug': 'gorillaz'
+            });
+
+            expect(selectors[2]).toEqual({
+                'type': 'music/album',
+                'keys.item.slug': 'humanz',
+
+                'artist.keys.test.id': '1'
+            });
+
+            expect(selectors[3]).toEqual({
+                'type': 'music/album',
+                'keys.item.slug': 'humanz',
+
+                'artist.keys.item.slug': 'gorillaz'
+            });
+        });
+    });
+});
