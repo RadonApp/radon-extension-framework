52dc1a465432d2e8b83824149c22325fadb61bf9
Dean Gardiner <me@dgardiner.net>
2018-01-17 14:20:47 +1300

    Added `item` properties to the `OptionComponent` class

--- src/services/configuration/components/options/base.js
+++ src/services/configuration/components/options/base.js
@@ -2,5 +2,19 @@ import React from 'react';
 
 
 export default class OptionComponent extends React.Component {
+    get id() {
+        return this.props.item && this.props.item.id;
+    }
 
+    get plugin() {
+        return this.props.item && this.props.item.plugin;
+    }
+
+    get preferences() {
+        return this.props.item && this.props.item.preferences;
+    }
 }
+
+OptionComponent.defaultProps = {
+    item: null
+};
