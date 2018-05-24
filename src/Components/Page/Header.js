a20380844fc8df56805492382d2ac188e72728c5
Dean Gardiner <me@dgardiner.net>
2018-05-25 10:19:21 +1200

    Improved `PageHeader` alignment

--- src/Components/Page/Header.js
+++ src/Components/Page/Header.js
@@ -6,7 +6,7 @@ import './Header.scss';
 export default class PageHeader extends React.Component {
     render() {
         return (
-            <div className="PageHeader columns">
+            <div className="PageHeader expanded row columns">
                 <div className="row">
                     <div className="PageHeader-title small-10 columns">
                         <h3>{this.props.title}</h3>
